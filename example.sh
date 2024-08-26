cdc0d8fa8300aba458dcb16f524fab69c39e6eb5
Mass23 <massimo.bourquin@gmail.com>
2024-08-26 11:20:14 +0000

    tested yaml file

--- example.sh
+++ example.sh
@@ -1,2 +1,2 @@
-python3 process_16S_nanopore.py -f pipeline_test_data/ -n pipeline_test -m metadata_test.tsv -t 72 --skippreprocessing
+python3 process_16S_nanopore.py -f pipeline_test_data/ -n pipeline_test -m metadata_test.tsv -t 24 --skippreprocessing
 
