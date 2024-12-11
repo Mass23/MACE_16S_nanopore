d275b6af69bb59bc4f7874ca8b09d9ae8e22903b
Mass23 <33321080+Mass23@users.noreply.github.com>
2024-12-11 11:54:42 +0100

    Update example.sh

--- example.sh
+++ example.sh
@@ -1,2 +1,2 @@
-python3 process_16S_nanopore.py -f /data/16s/test_set/fastq_files/ -n /data/16s/test_set/test_run -m /data/16s/test_set/meta_test.csv -t 32 -c /data/databases/16S_classifiers/nov_2024/silva/silva-138-99-nb-classifier.qza --skippreprocessing ##--skipqiime2 #--skippreprocessing
+python3 process_16S_nanopore.py -f /data/16s/test_set/fastq_files/ -n /data/16s/processed_data/test_set -m /data/16s/test_set/meta_test.csv -t 32 -c /data/databases/16S_classifiers/nov_2024/silva/silva-138-99-nb-classifier.qza #--skippreprocessing --skipqiime2
 
